15f6fe3ddee46b15ea685a90245efde814340a38
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2013-09-20 22:17:53 +0800

    Update Validate Binary Search Tree.cc

--- Leetcode/Validate Binary Search Tree.cc
+++ Leetcode/Validate Binary Search Tree.cc
@@ -1,27 +1,24 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool isValidBST(TreeNode *root) {
-        _root = root;
-        return _isValidBST(_root);
-    }
-    bool _isValidBST(TreeNode *root) {
-        if (!root) return true;
-        if (_search(root -> val, _root) == root) {
-            return _isValidBST(root -> left) && _isValidBST(root -> right);
-        }
-        return false;
+        return !root || 
+                _isValidBST(root -> left, INT_MIN, root -> val) && 
+                _isValidBST(root -> right, root -> val, INT_MAX);
     }
     
-    TreeNode* _search(int val, TreeNode* root) {
-        if (!root) return false;
-        
-        if (root -> val > val) {
-            return _search(val, root -> left);
-        } else if (root -> val < val) {
-            return _search(val, root -> right);
-        } else {
-            return root;
-        }
+    bool _isValidBST(TreeNode *root, int mini, int maxi) {
+        if (not root) return true;
+        else if (not (mini < root -> val && root -> val < maxi)) return false;
+        else return _isValidBST(root -> left, mini, root -> val) && 
+                    _isValidBST(root -> right, root -> val, maxi);
     }
-    TreeNode* _root;
 };
