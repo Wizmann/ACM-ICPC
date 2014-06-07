96f3e7b539854ec9b2f626ebbf09c8248a39017b
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2014-06-07 21:44:53 +0800

    Update Interleaving String.cc

--- Leetcode/Interleaving String.cc
+++ Leetcode/Interleaving String.cc
@@ -1,33 +1,34 @@
-const int SIZE = 1024;
-char dp[SIZE][SIZE];
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        memset(dp, 0, sizeof(dp));
-        dp[0][0] = 1;
-        if (s1.size() + s2.size() != s3.size()) {
-            return false;
+        vector<vector<int> > dp;
+        int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
+        
+        dp.resize(l1 + 5);
+        for (int i = 0; i < (int)dp.size(); i++) {
+            dp[i].resize(l2 + 5);
+            fill(dp[i].begin(), dp[i].end(), -1);
         }
-        for (int i = 0; i < s1.size(); i++) {
-            if (s1[i] == s3[i]) {
-                dp[i+1][0] = 1;
-            } else {
-                break;
+        
+        dp[0][0] = 0;
+        for (int i = 0; i <= l1; i++) {
+            for (int j = 0; j <= l2; j++) {
+                if (i - 1 >= 0 && dp[i - 1][j] != -1) {
+                    int p3 = dp[i - 1][j];
+                    if (s1[i - 1] == s3[p3]) {
+                        dp[i][j] = p3 + 1;
+                    }
+                }
+                
+                if (j - 1 >= 0 && dp[i][j - 1] != -1) {
+                    int p3 = dp[i][j - 1];
+                    if (s2[j - 1] == s3[p3]) {
+                        dp[i][j] = p3 + 1;
+                    }
+                }
             }
         }
-        for (int i = 0; i < s2.size(); i++) {
-            if (s2[i] == s3[i]) {
-                dp[0][i+1] = 1;
-            } else {
-                break;
-            }
-        }
-        for (int i = 0; i < s1.size(); i++) {
-            for (int j = 0; j < s2.size(); j++) {
-                dp[i+1][j+1] = (dp[i][j+1] && s1[i] == s3[i+j+1]) ||
-                               (dp[i+1][j] && s2[j] == s3[i+j+1]);
-            }
-        }
-        return dp[s1.size()][s2.size()];
+        
+        return dp[l1][l2] == l3;
     }
 };
