dab7be812d8b18cf314b21675541090c205ba168
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2014-06-11 20:17:55 +0800

    Update Symmetric Tree.cc

--- Leetcode/Symmetric Tree.cc
+++ Leetcode/Symmetric Tree.cc
@@ -31,3 +31,50 @@ public:
         return isSymmetric(ll -> left, rr -> right) && isSymmetric(ll -> right, rr -> left);
     }
 };
+
+// ---
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (!root) {
+            return true;
+        }
+        stack<TreeNode*> ll, rr;
+        ll.push(root -> left);
+        rr.push(root -> right);
+        while (true) {
+            if (ll.empty() && rr.empty()) {
+                break;
+            }
+            TreeNode *lp = ll.top();
+            ll.pop();
+            TreeNode *rp = rr.top();
+            rr.pop();
+            
+            if (!lp && !rp) {
+                continue;
+            }
+            if (!lp || !rp) {
+                return false;
+            }
+            if (lp -> val != rp -> val) {
+                return false;
+            }
+            ll.push(lp -> left);
+            ll.push(lp -> right);
+            rr.push(rp -> right);
+            rr.push(rp -> left);
+        }
+        return true;
+    }
+};
