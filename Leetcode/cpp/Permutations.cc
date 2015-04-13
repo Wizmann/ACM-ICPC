8e8da45fd46b6a92e83f4c16e17d4f1c171259c4
Wizmann <mail.kuuy@gmail.com>
2015-04-14 01:56:08 +0800

    add something

--- Leetcode/cpp/Permutations.cc
+++ Leetcode/cpp/Permutations.cc
@@ -1,33 +1,32 @@
 class Solution {
 public:
     vector<vector<int> > permute(vector<int> &num) {
-        int n = num.size();
         sort(num.begin(), num.end());
         vector<vector<int> > res;
-        while (true) {
+        do {
             res.push_back(num);
-            bool flag = false;
-            for (int i = n - 2; i >= 0; i--) {
-                int ii = i + 1;
-                if (num[i] < num[ii]) {
-                    swap(num[i], num[get_max_pos(i, num)]);
-                    reverse(num.begin() + ii, num.end());
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                break;
-            }
-        }
+        } while (_next_permutation(num));
         return res;
     }
-    int get_max_pos(int pos, vector<int>& num) {
-        for (int i = num.size() - 1; i > pos; i--) {
-            if (num[i] > num[pos]) {
-                pos = i;
+private:
+    bool _next_permutation(vector<int>& num) {
+        int n = num.size();
+        for (int i = n - 2; i >= 0; i--) {
+            if (num[i] < num[i + 1]) {
+                _swap_the_minimum(num, i, n);
+                reverse(num.begin() + i + 1, num.end());
+                return true;
             }
         }
-        return pos;
+        return false;
+    }
+    
+    void _swap_the_minimum(vector<int>& num, int pos, int n) {
+        for (int i = n - 1; i >= pos; i--) {
+            if (num[i] > num[pos]) {
+                swap(num[i], num[pos]);
+                return;
+            }
+        }
     }
 };
