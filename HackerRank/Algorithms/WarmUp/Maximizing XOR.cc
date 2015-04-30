f991a7a11a675413bf076b84dbb4ef3cef05251e
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2015-04-30 10:18:26 +0800

    Create Maximizing XOR.cc

---
+++ HackerRank/Algorithms/WarmUp/Maximizing XOR.cc
@@ -0,0 +1,47 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+    int res = 0;
+    int i = 0;
+    while (r > l) {
+        int u = (r - l >= 1)? 1: 0;
+        res |= (u << i);
+        r >>= 1;
+        l >>= 1;
+        i++;
+    }
+    return res;
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}
