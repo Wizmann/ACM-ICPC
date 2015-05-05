31c9e2e4d9eb7d6257d846177b15a75c075c2a03
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2015-05-05 17:19:14 +0800

    Create Reverse Linked List.cc

---
+++ LintCode/cpp/Reverse Linked List.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition of ListNode
+ * 
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ * 
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    ListNode *reverse(ListNode *head) {
+        return do_reverse(nullptr, head);
+    }
+private:
+    ListNode *do_reverse(ListNode *prev, ListNode *cur) {
+        if (cur == nullptr) {
+            return prev;
+        }
+        ListNode* next = cur->next;
+        cur->next = prev;
+        return do_reverse(cur, next);
+    }
+};
+
