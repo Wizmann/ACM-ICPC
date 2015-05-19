53b498f863eb147cb069f4279c6ab9de36ff5b08
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2015-05-19 20:54:28 +0800

    Create Matrix Zigzag Traversal.cc

---
+++ LintCode/cpp/Matrix Zigzag Traversal.cc
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    /**
+     * @param matrix: a matrix of integers
+     * @return: a vector of integers
+     */
+    vector<int> printZMatrix(vector<vector<int> > &matrix) {
+        int cnt = 0;
+        vector<int> res;
+        
+        int n = matrix.size();
+        if (n == 0) {
+            return res;
+        }
+        int m = matrix[0].size();
+        if (m == 0) {
+            return res;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            int y = 0;
+            int x = i;
+            
+            vector<int> tmp;
+            
+            while (true) {
+                tmp.push_back(matrix[y][x]);
+                y += 1;
+                x -= 1;
+                if (x < 0 || y < 0 || x >= m || y >= n) {
+                    break;
+                }
+            }
+            
+            if (cnt % 2 == 0) {
+                reverse(tmp.begin(), tmp.end());
+            }
+            copy(tmp.begin(), tmp.end(), back_inserter(res));
+            cnt++;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            int y = i;
+            int x = m - 1;
+            
+            vector<int> tmp;
+            
+            while (true) {
+                tmp.push_back(matrix[y][x]);
+                y += 1;
+                x -= 1;
+                if (x < 0 || y < 0 || x >= m || y >= n) {
+                    break;
+                }
+            }
+            
+            if (cnt % 2 == 0) {
+                reverse(tmp.begin(), tmp.end());
+            }
+            copy(tmp.begin(), tmp.end(), back_inserter(res));
+            cnt++;
+        }
+        
+        return res;
+    }
+};
