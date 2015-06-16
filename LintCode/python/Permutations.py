2b829321f85dd10ce4e6e96b2c5b2e3a2d683bb9
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2015-06-16 14:41:03 +0800

    Create Permutations.py

---
+++ LintCode/python/Permutations.py
@@ -0,0 +1,31 @@
+class Solution:
+    """
+    @param nums: A list of Integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+        if not nums:
+            return []
+        n = len(nums)
+        nums.sort()
+        res = []
+        
+        while True:
+            res.append(nums[:])
+            p = -1
+            for i in xrange(n - 2, -1, -1):
+                if nums[i] < nums[i + 1]:
+                    p = i
+                    break
+            if p == -1:
+                break
+            q = -1
+            v = 0xdeadbeef
+            for i in xrange(p + 1, n):
+                if nums[i] > nums[p] and nums[i] < v:
+                    q = i
+                    v = nums[i]
+            nums[p], nums[q] = nums[q], nums[p]
+            nums[p + 1:] = nums[p + 1:][::-1]
+            
+        return res
