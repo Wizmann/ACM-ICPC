4a7ed17ef42f7968efabb78f2b44ef399e7bbf9d
Wizmann <mail.kuuy@gmail.com>
2015-11-19 00:07:08 +0800

    no, not again

--- Leetcode/Algorithm/cpp/First Bad Version.cc
+++ Leetcode/Algorithm/cpp/First Bad Version.cc
@@ -2,15 +2,14 @@
 bool isBadVersion(int version);
 
 class Solution {
-    typedef long long llint;
 public:
     int firstBadVersion(int n) {
-        llint l = 1;
-        llint r = n;
-        
+        int l = 0;
+        int r = n;
         while (l <= r) {
-            llint m = (l + r) >> 1;
-            if (isBadVersion(m)) {
+            int m = (l >> 1) + (r >> 1) + (l & r & 1);
+            bool res = isBadVersion(m);
+            if (res) {
                 r = m - 1;
             } else {
                 l = m + 1;
