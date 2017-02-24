1fee847278c2da70be11a1041ee38a44c5cc55bc
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2017-02-24 21:01:39 +0800

    Update Introduction to Sets.py

--- HackerRank/Python/Sets/Introduction to Sets.py
+++ HackerRank/Python/Sets/Introduction to Sets.py
@@ -1,3 +1,11 @@
+from __future__ import division
+
 def average(array):
     s = set(array)
     return 1.0 * sum(s) / len(s)
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    result = average(arr)
+    print result
