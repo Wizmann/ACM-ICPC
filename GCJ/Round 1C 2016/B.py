939ac672c5d88d888566573bc769c28a8a40e595
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2017-03-21 17:18:05 +0800

    Update and rename B.small.py to B.py

--- GCJ/Round 1C 2016/B.py
+++ GCJ/Round 1C 2016/B.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def __init__(self, b, m):
         self.b = b
         self.m = m
@@ -58,10 +58,58 @@ class Solution(object):
             path_cnt = self.dfs(g, 0)
 
             if path_cnt == self.m:
-                print g
                 return g
 
         return None
+
+class Solution2(object):
+    def __init__(self, b, m):
+        self.b = b
+        self.m = m
+
+    def log2(self, n):
+        cnt = 0
+        while n:
+            cnt += 1
+            n >>= 1
+        return cnt
+
+    def solve(self):
+        if self.m > (1 << (self.b - 2)):
+            return None
+
+        mat = [ [0 for i in xrange(self.b) ] for j in xrange(self.b)]
+
+        l = self.log2(self.m)
+        for i in xrange(l):
+            for j in xrange(i + 1, l):
+                mat[i][j] = 1
+            mat[i][self.b - 1] = 1
+
+        self.m -= (1 << (l - 1))
+        if self.m:
+            mat[l][self.b - 1] = 1
+
+        for i in xrange(1, self.b):
+            if self.m & (1 << (i - 1)):
+                mat[i][l] = 1
+
+        return mat
+
+def test():
+    for i in xrange(2, 6):
+        for j in xrange(1, 25):
+            S1 = Solution1(i, j)
+            S2 = Solution2(i, j)
+
+            ans = S2.solve()
+            print i, j, ans
+            if not ans:
+                assert S1.solve() == None
+            else:
+                print S1.dfs(ans, 0)
+                assert S1.dfs(ans, 0) == j
+
 
 if __name__ == '__main__':
     from multiprocessing import Pool
@@ -70,7 +118,7 @@ if __name__ == '__main__':
     T = int(raw_input())
     queries = [map(int, raw_input().split()) for i in xrange(T)]
 
-    ans = p.map(lambda (x, y): Solution(x, y).solve(), queries)
+    ans = p.map(lambda (x, y): Solution2(x, y).solve(), queries)
 
     for i, mat in enumerate(ans):
         print 'Case #%d: %s' % (i + 1, 'POSSIBLE' if mat else 'IMPOSSIBLE')
