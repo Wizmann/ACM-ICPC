c59a7bbf9663f09dc167e5d68f8dafdedad77eaf
Wizmann <mail.kuuy@gmail.com>
2017-03-31 22:48:23 +0800

    add a mess

--- Exemplars/数据结构/数状数组/「树状数组」在线求区间最大值.cc
+++ Exemplars/数据结构/数状数组/「树状数组」在线求区间最大值.cc
@@ -1,45 +1,45 @@
-struct BIT_RMQ
+struct BiTreeRMQ
 {
-	int baum[SIZE],num[SIZE];
-	inline int lowbit(int x)
-	{
-		return x&(-x);
-	}
-	void init()
-	{
-		memset(baum,0,sizeof(baum));
-		memset(num,0,sizeof(num));
-	}
-	int query(int a,int b)
-	{
-		int ans=num[b];
-		while(1)
-		{
-			ans=max(ans,num[b]);
-			if(a==b) break;
-			else
-			{
-				b--;
-				while(b-a>=lowbit(b))
-				{
-					ans=max(ans,baum[b]);
-					b-=lowbit(b);
-				}
-			}
-		}
-		return ans;
-	}
+    int baum[SIZE],num[SIZE];
+    inline int lowbit(int x)
+    {
+        return x&(-x);
+    }
+    void init()
+    {
+        for(int i=1;i<SIZE;i++)
+        {
+            baum[i]=num[i];
+            for(int j=1;j<lowbit(i);j<<=1)
+            {
+                baum[i]=min(baum[i],baum[i-j]);
+            }
+        }
+    }
+    int query(int a,int b)
+    {
+        int ans=num[b];
+        while(a != b)
+        {
+            for (b = b - 1; b - lowbit(b) >= a; b -= lowbit(b))
+            {
+                ans=min(ans,baum[b]);
+            }
+            ans = min(ans, num[b]);
+        }
+        return ans;
+    }
 
-	void modify(int pos,int val)
-	{
-		num[pos]=val;
-		for(int i=pos;i<SIZE;i+=lowbit(i))
-		{
-			baum[i]=max(val,baum[i]);
-			for(int j=1;j<lowbit(i);j<<=1)
-			{
-				baum[i]=max(baum[i],baum[i-j]);
-			}
-		}
-	}
+    void modify(int pos,int val)
+    {
+        num[pos]=val;
+        for(int i=pos;i<SIZE;i+=lowbit(i))
+        {
+            baum[i]=num[i];
+            for(int j=1;j<lowbit(i);j<<=1)
+            {
+                baum[i]=min(baum[i],baum[i-j]);
+            }
+        }
+    }
 };
