42b35eab4db82b33e134bc5ae13181758558933a
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2018-02-17 20:44:56 +0800

    Create Nth Highest Salary.sql

---
+++ Leetcode/Database/Nth Highest Salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE M INT;
+  SET M=N-1;
+  RETURN (
+      SELECT DISTINCT Salary FROM Employee
+      ORDER BY Salary DESC
+      LIMIT M, 1
+  );
+END
