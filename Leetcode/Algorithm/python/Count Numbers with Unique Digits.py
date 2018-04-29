c95be9acf083c5d5fbe1a9ed8cb12c032beecc72
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2018-04-29 16:59:47 +0800

    Update Count Numbers with Unique Digits.py

--- Leetcode/Algorithm/python/Count Numbers with Unique Digits.py
+++ Leetcode/Algorithm/python/Count Numbers with Unique Digits.py
@@ -10,3 +10,30 @@ class Solution(object):
             return 1
         return A(n, 10) - A(n - 1, 9) + self.countNumbersWithUniqueDigits(n - 1)
         
+#####
+
+N = (1 << 10)
+
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        dp = [[0 for i in xrange(N)] for j in xrange(2)]
+        dp[0][0] = 1
+        
+        p = 0
+        res = 0
+        for i in xrange(n):
+            q = p ^ 1
+            dp[q] = [0 for i in xrange(N)]
+            for j in xrange(N):
+                for k in xrange(1, 10):
+                    if j & (1 << k):
+                        continue
+                    u = j | (1 << k)
+                    dp[q][u] += dp[p][j]
+            res += sum(dp[q])
+            for j in xrange(N):
+                if j & 1:
+                    continue
+                dp[q][j | 1] += dp[p][j]
+            p = q
+        return res + 1
