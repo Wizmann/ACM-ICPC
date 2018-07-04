6fbe49de434d91918a5a5f9a5f888fc8b28617e5
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2018-07-04 19:04:54 +0800

    Update Buddy Strings.py

--- Leetcode/Algorithm/python/Buddy Strings.py
+++ Leetcode/Algorithm/python/Buddy Strings.py
@@ -2,7 +2,7 @@
 [Description]
 Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
 
-[Tag]
+[Tags]
 string, implementation
 
 [Difficulty]
