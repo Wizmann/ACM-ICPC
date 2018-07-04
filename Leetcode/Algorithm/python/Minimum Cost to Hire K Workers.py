dfbb7a42aeb7b5fbeb27e863b0e7404dc5c03142
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2018-07-04 19:18:20 +0800

    Update Minimum Cost to Hire K Workers.py

--- Leetcode/Algorithm/python/Minimum Cost to Hire K Workers.py
+++ Leetcode/Algorithm/python/Minimum Cost to Hire K Workers.py
@@ -1,3 +1,24 @@
+'''$$$
+[Description]
+There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
+
+Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
+
+* Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+* Every worker in the paid group must be paid at least their minimum wage expectation.
+
+Return the least amount of money needed to form a paid group satisfying the above conditions.
+
+[Tags]
+greedy, heap
+
+[Difficulty]
+3.5
+
+[Url]
+https://leetcode.com/problems/minimum-cost-to-hire-k-workers/description/
+$$$'''
+
 import heapq
 
 INF = 10 ** 10
@@ -19,7 +40,3 @@ class Solution(object):
             if len(pq) == K:
                 res = min(res, -p * sum(pq))    
         return res
-            
-        
-            
-        
