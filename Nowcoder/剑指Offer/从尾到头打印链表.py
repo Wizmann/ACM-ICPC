21cf859f37218331320b9f88486de1aee25340c1
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2019-02-23 13:34:42 +0800

    Create 从尾到头打印链表.py

---
+++ Nowcoder/剑指Offer/从尾到头打印链表.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        res = []
+        while listNode:
+            res.append(listNode.val)
+            listNode = listNode.next
+        return res[::-1]
