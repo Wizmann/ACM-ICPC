8bcf16ad88c4c5be6c47c2dc70a3a33c80d4abf8
Moody Wizmann <mail.kuuy@gmail.com>
2020-02-11 19:18:39 +0800

    Create Check If N and Its Double Exist.py

---
+++ Leetcode/Algorithm/python/Check If N and Its Double Exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        if arr.count(0) >= 2:
+            return True
+        st = set(filter(lambda x: x, arr) )
+        for num in arr:
+            if 2 * num in st:
+                return True
+        return False
