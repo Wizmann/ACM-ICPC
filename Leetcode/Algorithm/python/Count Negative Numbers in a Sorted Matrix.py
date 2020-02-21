f6392b6ff5aab1a63052a0043def624cd6afd346
Moody Wizmann <mail.kuuy@gmail.com>
2020-02-21 12:21:13 +0800

    Create Count Negative Numbers in a Sorted Matrix.py

---
+++ Leetcode/Algorithm/python/Count Negative Numbers in a Sorted Matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        
+        x = m - 1
+        
+        tot = 0
+        for i in range(n):
+            while x >= 0 and grid[i][x] < 0:
+                x -= 1
+            #print(m, x, m - x - 1)
+            tot += m - x - 1
+        return tot
