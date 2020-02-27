316a05122dd281331375156ea533cfd6eaf55fa1
Moody Wizmann <mail.kuuy@gmail.com>
2020-02-28 00:51:29 +0800

    Create Sort Integers by The Number of 1 Bits.py

---
+++ Leetcode/Algorithm/python/Sort Integers by The Number of 1 Bits.py
@@ -0,0 +1,9 @@
+from typing import *
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        count1 = lambda num: ('{:b}'.format(num).count('1'), num)
+        return sorted(arr, key=count1)
+
+S = Solution()
+assert S.sortByBits([2, 1]) == [1, 2]
+assert S.sortByBits([1]) == [1]
