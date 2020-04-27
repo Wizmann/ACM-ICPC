dab11a029747f34ea90f3939a693e2a1c97da94d
Moody Wizmann <mail.kuuy@gmail.com>
2020-04-27 22:43:53 +0800

    Update Diagonal Traverse II.py

--- Leetcode/Algorithm/python/Diagonal Traverse II.py
+++ Leetcode/Algorithm/python/Diagonal Traverse II.py
@@ -3,12 +3,14 @@ from collections import defaultdict
 class Solution(object):
     def findDiagonalOrder(self, nums):
         d = defaultdict(list)
-        
+        maxi = 0        
         for i, line in enumerate(nums):
             for j, num in enumerate(line):
                 u = i + j
                 d[u].append(num)
+                maxi = max(maxi, u)
         res = []
-        for key in sorted(d.keys()):
-            res += d[key][::-1]
+        for i in xrange(maxi + 1):
+            res += d[i][::-1]
         return res
+            
