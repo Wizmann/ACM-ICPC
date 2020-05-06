bd3141fc2acd9cf42068a0f5ff01b55d2d3f9e7d
Moody Wizmann <mail.kuuy@gmail.com>
2020-05-06 20:50:30 +0800

    Update Common IO.cc

--- Exemplars/Common IO.cc
+++ Exemplars/Common IO.cc
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+typedef long long llint;
+
 void print() { cout << "\n"; }
 
 template <typename...T, typename X>
@@ -24,10 +26,33 @@ void input(X& x, T&... args) { cin >> x; input(args...); }
 
 int main()
 {
-    int a, b;
-    string c;
-    float d;
-    input(a, b, c, d);
-    print(a, b, c, d);
+    int n;
+    input(n);
+
+    vector<int> ns(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ns[i]);
+    }
+
+    map<int, int> mp;
+    for (auto num: ns) {
+        mp[num]++;
+    }
+
+    llint tot = 0;
+    for (auto kv: mp) {
+        tot += 1LL * kv.second * (kv.second - 1) / 2;
+    }
+
+    for (int i = 0; i < n; i++) {
+        llint u = tot;
+        int cur = ns[i];
+        int cnt = mp[cur];
+        u -= 1LL * cnt * (cnt - 1) / 2;
+        u += 1LL * (cnt - 1) * (cnt - 2) / 2;
+        puts(to_string(u).c_str());
+    }
+
     return 0;
 }
+
