969de7df763fcad2f2280941fd54aa019f26b25a
Moody Wizmann <mail.kuuy@gmail.com>
2020-05-06 11:29:15 +0800

    Create Common IO.cc

---
+++ Exemplars/Common IO.cc
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+void print() { cout << "\n"; }
+
+template <typename...T, typename X>
+void print(X& x, T... args) { cout << x << " "; print(args...); }
+
+void input() { // pass }
+
+template <typename...T, typename X>
+void input(X& x, T&... args) { cin >> x; input(args...); }
+
+int main()
+{
+    int a, b;
+    string c;
+    float d;
+    input(a, b, c, d);
+    print(a, b, c, d);
+    return 0;
+}
