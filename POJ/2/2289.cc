46ca537c888d8909a752d402ed93a35b0a09e77f
Moody Wizmann <mail.kuuy@gmail.com>
2020-05-16 16:44:39 +0800

    Update 2289.cc

--- POJ/2/2289.cc
+++ POJ/2/2289.cc
@@ -84,3 +84,152 @@ int main()
 	}
 	return 0;
 }
+
+////
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <cassert>
+
+using namespace std;
+
+typedef long long llint;
+
+const int SIZE = 1111;
+
+vector<string> split_string(const string& line, char delim) {
+    std::istringstream stream(line);
+    string token;
+    vector<string> res;
+    while (std::getline(stream, token, delim)) {
+        res.push_back(token);
+    }
+    return res;
+}
+
+class MultiHungary {
+public:
+    MultiHungary(int left_, int right_) : left(left_), right(right_) {
+        g = vector<vector<int> >(left);
+        for (int i = 0; i < left; i++) {
+            g[i] = vector<int>(right, 0);
+        }
+        caps = vector<int>(right, 0);
+    }
+
+    void setCapacity(int idx, int cap) {
+        caps[idx] = cap;
+    }
+
+    void addEdge(int l, int r) {
+        g[l][r] = 1;
+    }
+    
+
+    // `all_match` indicates if all nodes on the left 
+    // are matched with one of the right nodes
+    int match(bool& match_all_left, bool must_match_all_left) {
+        int res = 0;
+        match_all_left = true;
+        pre = vector<vector<int> >(right);
+        for (int i = 0; i < left; i++) {
+            visited = vector<bool>(right, false);
+            if (find_path(i)) {
+                res++;
+            } else {
+                match_all_left = false;
+            }
+
+            if (must_match_all_left && !match_all_left) {
+                return -1;
+            }
+        }
+
+        return res;
+    }
+private:
+    bool find_path(int cur) {
+        for (int i = 0; i < right; i++) {
+            if (g[cur][i] && !visited[i]) {
+                visited[i] = true;
+                if (pre[i].size() < caps[i]) {
+                    pre[i].push_back(cur);
+                    return true;
+                }
+                for (int j = 0; j < pre[i].size(); j++) {
+                    int& p = pre[i][j];
+                    if (find_path(p)) {
+                        p = cur;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+private:
+    int left, right;
+    vector<vector<int> > g;
+    vector<int> caps;
+    vector<vector<int> > pre;
+    vector<bool> visited;
+};
+
+char buffer[2048];
+
+int main() {
+    int n, m;
+
+    while (scanf("%d%d\n", &n, &m) != EOF) {
+        if (n + m == 0) {
+            break;
+        }
+
+        MultiHungary mh(n, m);
+
+        for (int i = 0; i < n; i++) {
+            fgets(buffer, 2048, stdin);
+            string line(buffer);
+            // puts(line.c_str());
+            vector<string> tokens = split_string(line, ' ');
+            int tn = tokens.size();
+            for (int j = 1; j < tn; j++) {
+                int g = atoi(tokens[j].c_str());
+                mh.addEdge(i, g);
+            }
+        }
+
+        // binary search
+        int l = 1;
+        int r = n;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            for (int i = 0; i < m; i++) {
+                mh.setCapacity(i, mid);
+            }
+
+            bool match_all_left = true;
+            mh.match(match_all_left, true);
+            if (match_all_left) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        printf("%d\n", l);
+    }
+    return 0;
+}
+
