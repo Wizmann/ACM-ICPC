bc6d6225dc4a0a30b8f5a015cd573c692d5c257e
Moody Wizmann <mail.kuuy@gmail.com>
2020-11-07 17:41:32 +0800

    Update T49111-攀登山峰.cc

--- Jisuanke/T49111-攀登山峰.cc
+++ Jisuanke/T49111-攀登山峰.cc
@@ -1,3 +1,4 @@
+// 概率骗分解法
 #pragma G++ optimize("O2")
 
 #include <cstdio>
@@ -216,3 +217,137 @@ $$$TEST$$$
 $$$TEST$$$
 
 */
+
+// 主席树解法（正解）
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+#define print(x) cout << x << endl
+#define input(x) cin >> x
+
+const int N = 100010;
+
+const int INF = 0x3f3f3f3f;
+
+class PersistentSegTree {
+    struct TreeNode {
+        TreeNode(): sum(0), L_son(0), R_son(0) {}
+        int sum, L_son, R_son;
+    };
+
+public:
+    // Σ╕Çσ«ÜΦªüσ£¿σê¥σºïσîûµù╢τí«σ«Üvector<TreeNode>τÜäσñºσ░Å, σÉªσêÖΣ╝Üσ¢áΣ╕║mem re-allocΘÇáµêÉσÑçµÇ¬τÜäΘù«Θóÿ
+    // n * 19Σ╕║τ╗ÅΘ¬îσÇ╝∩╝îσÅ»µá╣µì«Θóÿτ¢«Φªüµ▒éΘÜÅµù╢µ¢┤µû░
+    PersistentSegTree(int n): cnt(0), tree(n * 19) {
+        // pass
+    }
+
+    int Insert(int pre_root, int pos, int L, int R) {
+        //Σ╗Äµá╣Φèéτé╣σ╛ÇΣ╕ïµ¢┤µû░σê░σÅ╢σ¡É∩╝îµû░σ╗║τ½ïσç║Σ╕ÇΦ╖»µ¢┤µû░τÜäΦèéτé╣∩╝îΦ┐Öµá╖σ░▒µÿ»Σ╕ÇΘóùµû░µáæΣ║åπÇé
+        int root = CreateNode(tree[pre_root].sum + 1, tree[pre_root].L_son, tree[pre_root].R_son);
+        if (L == R) {
+            return root;
+        }
+        int M = (L + R) >> 1;
+        if (pos <= M) {
+            tree[root].L_son = Insert(tree[pre_root].L_son, pos, L, M);
+        } else {
+            tree[root].R_son = Insert(tree[pre_root].R_son, pos, M + 1, R);
+        }
+        return root;
+    }
+
+    int Query(int start, int end, int L, int R, double t) {
+        if (L == R) {
+            if (tree[end].sum - tree[start].sum > t) {
+                return L;
+            } else {
+                return -1;
+            }
+        }
+
+        int res = -1;
+        int M = (L + R) >> 1;
+        if (tree[tree[end].L_son].sum - tree[tree[start].L_son].sum > t) {
+            res = max(res, Query(tree[start].L_son, tree[end].L_son, L, M, t));
+        }
+        if (tree[tree[end].R_son].sum - tree[tree[start].R_son].sum > t) {
+            res = max(res, Query(tree[start].R_son, tree[end].R_son, M + 1, R, t));
+        }
+
+        return res;
+    }
+
+private:
+    inline int CreateNode(int sum, int L_son, int R_son) {
+        int idx = ++cnt;
+        tree[idx].sum = sum;
+        tree[idx].L_son = L_son;
+        tree[idx].R_son = R_son;
+        return idx;
+    }
+
+public:
+    int cnt;
+    vector<TreeNode> tree;
+};
+
+
+//Φ«░σ╜òσÄƒµò░τ╗äπÇüµÄÆσ║ÅσÉÄτÜäµò░τ╗ä
+int nums[N], sorted[N];
+
+int main()
+{
+    int n, m;
+    input(n >> m);
+    auto pst = PersistentSegTree(n);
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &nums[i]);
+        sorted[i] = nums[i];
+    }
+    sort(sorted + 1, sorted + 1 + n);
+    int num = unique(sorted + 1, sorted + n + 1) - (sorted + 1);
+
+    // µ»ÅΣ╕¬σàâτ┤áσ»╣σ║öτÜäµá╣Φèéτé╣
+    vector<int> root(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        //σ«₧ΘÖàΣ╕èµÿ»σ»╣µ»ÅΣ╕¬σàâτ┤áσ╗║τ½ïΣ║åΣ╕ÇΘóùτ║┐µ«╡µáæ∩╝îΣ┐¥σ¡ÿσà╢µá╣Φèéτé╣
+        int pos = lower_bound(sorted + 1, sorted + num + 1, nums[i]) - sorted;
+        root[i] = pst.Insert(root[i - 1], pos, 1, num);
+    }
+    int l, r, k;
+    while (m--) {
+        scanf("%d%d%d", &l, &r, &k);
+        int pos = pst.Query(root[l - 1], root[r], 1, num, 1.0 * (r - l + 1) / k);
+        if (pos == -1) {
+            puts("-1");
+        } else {
+            printf("%d\n", sorted[pos]);
+        }
+    }
+    return 0;
+}
+
+/*
+^^^TEST^^^
+8 3
+1 2 1 4 4 5 3 3 
+3 7 5
+1 4 3
+3 8 6
+----
+4
+1
+4
+$$$TEST$$$
+*/
+
