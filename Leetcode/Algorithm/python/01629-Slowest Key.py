7c6cee7fa778c7897273316495aac7936fac2832
Moody Wizmann <mail.kuuy@gmail.com>
2021-02-27 17:28:46 +0800

    Update 01629-Slowest Key.py

--- Leetcode/Algorithm/python/01629-Slowest Key.py
+++ Leetcode/Algorithm/python/01629-Slowest Key.py
@@ -1,8 +1,10 @@
 class Solution(object):
     def slowestKey(self, releaseTimes, keysPressed):
-        """
-        :type releaseTimes: List[int]
-        :type keysPressed: str
-        :rtype: str
-        """
-        
+        res = (-1, 'a')
+        n = len(keysPressed)
+        pre = 0
+        for i in xrange(n):
+            delta = releaseTimes[i] - pre
+            res = max(res, (delta, keysPressed[i]))
+            pre = releaseTimes[i]
+        return res[1]
