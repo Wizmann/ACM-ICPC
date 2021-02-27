ae9ff758dd511abbcf0e041a1c5cbd98916edfe5
Moody Wizmann <mail.kuuy@gmail.com>
2021-02-27 17:28:16 +0800

    Create 01629-Slowest Key.py

---
+++ Leetcode/Algorithm/python/01629-Slowest Key.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def slowestKey(self, releaseTimes, keysPressed):
+        """
+        :type releaseTimes: List[int]
+        :type keysPressed: str
+        :rtype: str
+        """
+        
