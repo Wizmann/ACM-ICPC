3560c12405dcdaa48d75b853b108c12ec821f3cb
Moody Wizmann <mail.kuuy@gmail.com>
2021-03-14 22:13:13 +0800

    Create 求地板除的和.cc

---
+++ Exemplars/数论/求地板除的和.cc
@@ -0,0 +1,17 @@
+llint floor_sum(llint n, llint m, llint a, llint b) {
+    llint ans = 0;
+    if (a >= m) {
+        ans += (n - 1) * n * (a / m) / 2;
+        a %= m;
+    }
+    if (b >= m) {
+        ans += n * (b / m);
+        b %= m;
+    }
+
+    llint y_max = (a * n + b) / m, x_max = (y_max * m - b);
+    if (y_max == 0) return ans;
+    ans += (n - (x_max + a - 1) / a) * y_max;
+    ans += floor_sum(y_max, a, m, (a - x_max % a) % a);
+    return ans;
+}
