097125c0b2187f9b9cb10fdf3cb6570d51c276cd
Moody Wizmann <mail.kuuy@gmail.com>
2022-03-27 15:40:48 +0800

    Update 「树状数组」在线求区间最大值.cc

--- Exemplars/数据结构/树状数组/「树状数组」在线求区间最大值.cc
+++ Exemplars/数据结构/树状数组/「树状数组」在线求区间最大值.cc
@@ -43,3 +43,56 @@ struct BiTreeRMQ
         }
     }
 };
+
+
+///////
+
+// 模板没验过
+
+template <typename T, class Compare=less<int> >
+class BITreeRMQ {
+public:
+    BITreeRMQ() {}
+    BITreeRMQ(int n_, const vector<T>& nums_) : n(n_), nums(nums_), tree(nums_) {
+        assert(nums.size() == n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < lowbit(i); j <<= 1) {
+                tree[i] = cmp(tree[i], tree[i - j]);
+            }
+        }
+    }
+
+    T query(int a, int b) {
+        T ans = nums[b];
+        while (a != b) {
+            for (b = b - 1; b - lowbit(b) >= a; b -= lowbit(b)) {
+                ans = cmp(ans, tree[b]);
+            }
+            ans = cmp(ans, nums[b]);
+        }
+        return ans;
+    }
+
+    void modify(int pos, T val) {
+        nums[pos] = val;
+        for (int i = pos; i < n; i += lowbit(i)) {
+            tree[i] = nums[i];
+            for (int j = 1; j < lowbit(i); j <<= 1) {
+                tree[i] = cmp(tree[i], tree[i - j]);
+            }
+        }
+    }
+private:
+    T cmp(T a, T b) {
+        return cmp_func(a, b)? a: b;
+    }
+
+    inline int lowbit(int x) {
+        return x&(-x);
+    }
+
+    vector<T> tree;
+    vector<T> nums;
+    Compare cmp_func = Compare();
+    int n;
+};
