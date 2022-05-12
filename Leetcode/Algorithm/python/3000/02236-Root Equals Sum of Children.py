cd1e465cbd2d80a904745fe5792c9876bb0c4d5b
Moody Wizmann <mail.kuuy@gmail.com>
2022-05-12 22:00:28 +0800

    Create 02236-Root Equals Sum of Children.py

---
+++ Leetcode/Algorithm/python/3000/02236-Root Equals Sum of Children.py
@@ -0,0 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def checkTree(self, root):
+        return root.val == root.left.val + root.right.val
