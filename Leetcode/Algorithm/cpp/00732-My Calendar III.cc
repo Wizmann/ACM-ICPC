27040e48eb869740537388c140a3ba463f164230
Moody Wizmann <mail.kuuy@gmail.com>
2022-06-06 13:37:07 +0800

    Update 00732-My Calendar III.cc

--- Leetcode/Algorithm/cpp/00732-My Calendar III.cc
+++ Leetcode/Algorithm/cpp/00732-My Calendar III.cc
@@ -25,3 +25,42 @@ private:
  * MyCalendarThree obj = new MyCalendarThree();
  * int param_1 = obj.book(start,end);
  */
+
+
+// ---
+
+class MyCalendarThree {
+public:
+    MyCalendarThree() {
+        // pass
+    }
+    
+    int book(int start, int end) {
+        // print(">>", start, end);
+        auto iter1 = lower_bound(pos.begin(), pos.end(), start);
+        iter1 = pos.insert(iter1, start);
+
+        auto iter2 = lower_bound(neg.begin(), neg.end(), end);
+        neg.insert(iter2, end);
+
+        auto iter3 = lower_bound(neg.begin(), neg.end(), start);
+
+        int maxi = 0;
+        while (iter1 != pos.end() && *iter1 < end) {
+            while (iter3 != neg.end() && *iter3 <= *iter1) {
+                iter3++;
+            }
+            int cnt = distance(pos.begin(), iter1) + 1 - distance(neg.begin(), iter3);
+            // print(*iter1, cnt);
+            maxi = max(maxi, cnt);
+            ++iter1;
+        }
+        // print(maxi);
+        res = max(res, maxi);
+        return res;
+    }
+private:
+    vector<int> pos;
+    vector<int> neg;
+    int res = 0;
+};
