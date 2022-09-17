06813058a9d7cb93cd6068a5f42b9540015386c8
Moody Wizmann <mail.kuuy@gmail.com>
2022-09-17 23:35:26 +0800

    Create A.py

---
+++ AtCoder/UNICORN Programming Contest 2022/A.py
@@ -0,0 +1,21 @@
+a, b, c, d = map(int, raw_input().split())
+
+print (a + b) * (c - d)
+print 'Takahashi'
+
+'''
+^^^^^^TEST^^^
+1 2 5 3
+------------
+6
+Takahashi
+$$$TEST$$$
+
+
+^^^^^^TEST^^^
+10 -20 30 -40
+------------
+-700
+Takahashi
+$$$TEST$$$
+'''
