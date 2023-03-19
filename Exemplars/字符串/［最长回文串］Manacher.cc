b7455035fb1aacded263a65091b1014d6d2b9ea3
Moody Wizmann <mail.kuuy@gmail.com>
2023-03-19 16:44:31 +0800

    Update ［最长回文串］Manacher.cc

--- Exemplars/字符串/［最长回文串］Manacher.cc
+++ Exemplars/字符串/［最长回文串］Manacher.cc
@@ -1,94 +1,181 @@
-/*
-Palindrome
-Time Limit: 15000MS		Memory Limit: 65536KB
-
-Description
-
-Andy the smart computer science student was attending an algorithms class when the professor asked the students a simple question, "Can you propose an efficient algorithm to find the length of the largest palindrome in a string?" 
-
-A string is said to be a palindrome if it reads the same both forwards and backwards, for example "madam" is a palindrome while "acm" is not. 
-
-The students recognized that this is a classical problem but couldn't come up with a solution better than iterating over all substrings and checking whether they are palindrome or not, obviously this algorithm is not efficient at all, after a while Andy raised his hand and said "Okay, I've a better algorithm" and before he starts to explain his idea he stopped for a moment and then said "Well, I've an even better algorithm!". 
-
-If you think you know Andy's final solution then prove it! Given a string of at most 1000000 characters find and print the length of the largest palindrome inside this string.
-
-Input
-
-Your program will be tested on at most 30 test cases, each test case is given as a string of at most 1000000 lowercase characters on a line by itself. The input is terminated by a line that starts with the string "END" (quotes for clarity). 
-Output
-For each test case in the input print the test case number and the length of the largest palindrome. 
-
-Sample Input
-
-abcbabcbabcba
-abacacbaaaab
-END
-
-Sample Output
-
-Case 1: 13
-Case 2: 6
-*/
-
-//Result:wizmann	3974	Accepted	42972K	3297MS	G++	879B
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <deque>
+#include <string>
+#include <cassert>
 
 using namespace std;
 
-#define print(x) cout<<x<<endl
-#define input(x) cin>>x
-#define SIZE 10000005
+typedef long long llint;
 
-int dp[SIZE];
+const int INF = 0x3f3f3f3f;
+const llint INFLL = 0x3f3f3f3f3f3f3f3fLL;
 
-int manacher(string& str)
-{
-	memset(dp,0,sizeof(dp));
-	string s;
-	int n=str.size();
-	int ans=0;
-	for(int i=0;i<n;i++)
-	{
-		s+='#';
-		s+=str[i];
-	}
-	s+='#';
-	n=(n<<1)+1;
-	int i=0,j=1,k;
-	while(i<n)
-	{
-		while(i-j>=0 && i+j<n && s[i-j]==s[i+j]) j++;
-		dp[i]=j-1;
-		k=1;
-		while(k<=dp[i] && dp[i]-k!=dp[i-k])
-		{
-			dp[i+k]=min(dp[i-k],dp[i]-k);
-			k++;
-		}
-		i+=k;
-		j=max(j-k,0);
-	}
-	for(i=0;i<n;i++) ans=max(ans,dp[i]);
-	return ans;
+void print() { cout << "\n"; }
+
+template <typename...T, typename X>
+void print(X&& x, T... args) { cout << x << " "; print(args...); }
+
+int input() { return 0; }
+
+template <typename...T, typename X>
+int input(X& x, T&... args) {
+    if (!(cin >> x)) return 0;
+    return input(args...) + 1;
 }
 
+const int SIZE = 123456;
 
-int main()
-{
-	int cas=1;
-	string str;
-	while(input(str))
-	{
-		if(str=="END") break;
-		else
-		{
-			printf("Case %d: ",cas++);
-			print(manacher(str));
-		}
-	}
-	return 0;
+class Manacher {
+public:
+    Manacher(const string& origin_) : origin(origin_) {
+        int n = origin.size();
+        for (int i = 0; i < n; i++) {
+            s += '#';
+            s += origin[i];
+        }
+
+        s += '#';
+        n = s.size();
+
+        d = vector<int>(n, 0);
+
+        for (int i = 0, j = 1, k; i < n; /* pass */) {
+            while (i - j >= 0 && i + j < n &&
+                   s[i-j] == s[i+j]) {
+                j++;
+            }
+            d[i] = j - 1;
+            k = 1;
+            while (k <= d[i] && d[i] - k != d[i - k]) {
+                d[i + k] = min(d[i - k], d[i] - k);
+                k++;
+            }
+            i += k;
+            j = max(j - k, 0);
+        }
+    }
+
+    int LongestPalindrome() const {
+        int maxi = 1;
+        for (auto num : d) {
+            maxi = max(maxi, num);
+        }
+        return maxi;
+    }
+
+    int LongestOddLenPalindrome(int pivot) const {
+        int idx = pivot * 2 + 1;
+        int left = idx / 2 - d[idx] / 2;
+        int right = left + d[idx] - 1;
+        return right - left + 1;
+    }
+
+    int LongestEvenLenPalindrome(int pivot) const {
+        int idx = pivot * 2 ;
+        int left = idx / 2 - d[idx] / 2;
+        int right = left + d[idx] - 1;
+        return right - left + 1;
+    }
+
+    int LongestPalindrome(int pivot) const {
+        return max(LongestOddLenPalindrome(pivot),
+                   LongestEvenLenPalindrome(pivot));
+    }
+
+private:
+    string origin;
+    string s;
+    vector<int> d;
+};
+
+string solve(const string& str) {
+    Manacher m(str);
+    int n = str.size();
+
+    set<int> st_l, st_r;
+    for (int i = 0; i < n; i++) {
+        // odd
+        {
+            int len = m.LongestOddLenPalindrome(i);
+            int left = i - len / 2;
+            int right = left + len - 1;
+
+            if (left == 0 && right != (int)str.size() - 1) {
+                // print("L", right);
+                st_l.insert(right);
+            }
+            if (right == (int)str.size() - 1 && left != 0) {
+                // print("R", left);
+                st_r.insert(left);
+            }
+        }
+
+        // even
+        {
+            int len = m.LongestEvenLenPalindrome(i);
+            int left = i - len / 2;
+            int right = left + len - 1;
+
+            if (left == 0 && right != (int)str.size() - 1) {
+                // print("L", right);
+                st_l.insert(right);
+            }
+            if (right == (int)str.size() - 1 && left != 0) {
+                // print("R", left);
+                st_r.insert(left);
+            }
+        }
+    }
+
+    int ans = str.size() - 1;
+    for (auto num : st_l) {
+        if (st_r.count(num + 1)) {
+            ans = min(ans, num);
+        }
+    }
+    return str.substr(0, ans + 1);
 }
+
+int main() {
+    int T;
+    input(T);
+
+    int n;
+    string s;
+    for (int case_ = 0; case_ < T; case_++) {
+        input(n);
+        input(s);
+
+        printf("Case #%d: %s\n", case_ + 1, solve(s).c_str());
+    }
+
+    return 0;
+}
+
+/*
+
+^^^TEST^^^
+3
+4
+abba
+4
+cccc
+6
+cdccdc
+-----
+Case #1: abba
+Case #2: c
+Case #3: cdc
+$$$TEST$$$
+
+*/
