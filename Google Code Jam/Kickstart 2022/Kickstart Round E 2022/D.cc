0239f2087c564d06c43545aecea94d9a1f43ac41
Moody Wizmann <mail.kuuy@gmail.com>
2023-03-19 16:31:32 +0800

    Update D.cc

--- Google Code Jam/Kickstart 2022/Kickstart Round E 2022/D.cc
+++ Google Code Jam/Kickstart 2022/Kickstart Round E 2022/D.cc
@@ -35,54 +35,105 @@ int input(X& x, T&... args) {
 
 const int SIZE = 123456;
 
-int dp[SIZE * 2];
-
-void manacher(const string& str) {
-    memset(dp,0,sizeof(dp));
-
-    string s;
-    int n = str.size();
-    for (int i = 0; i < n; i++) {
-        s += '#';
-        s += str[i];
-    }
-
-    s += '#';
-    n = s.size();
-
-    int i=0,j=1,k;
-
-    while(i<n)
-    {
-        while(i-j>=0 && i+j<n && s[i-j]==s[i+j]) j++;
-        dp[i]=j-1;
-        k=1;
-        while(k<=dp[i] && dp[i]-k!=dp[i-k])
-        {
-            dp[i+k]=min(dp[i-k],dp[i]-k);
-            k++;
+class Manacher {
+public:
+    Manacher(const string& origin_) : origin(origin_) {
+        int n = origin.size();
+        for (int i = 0; i < n; i++) {
+            s += '#';
+            s += origin[i];
         }
-        i+=k;
-        j=max(j-k,0);
+
+        s += '#';
+        n = s.size();
+
+        d = vector<int>(n, 0);
+
+        for (int i = 0, j = 1, k; i < n; /* pass */) {
+            while (i - j >= 0 && i + j < n &&
+                   s[i-j] == s[i+j]) {
+                j++;
+            }
+            d[i] = j - 1;
+            k = 1;
+            while (k <= d[i] && d[i] - k != d[i - k]) {
+                d[i + k] = min(d[i - k], d[i] - k);
+                k++;
+            }
+            i += k;
+            j = max(j - k, 0);
+        }
     }
-    // print(s);
+
+    int LongestPalindrome() const {
+        int maxi = 1;
+        for (auto num : d) {
+            maxi = max(maxi, num);
+        }
+        return maxi;
+    }
+
+    int LongestOddLenPalindrome(int pivot) const {
+        int idx = pivot * 2 + 1;
+        int left = idx / 2 - d[idx] / 2;
+        int right = left + d[idx] - 1;
+        return right - left + 1;
+    }
+
+    int LongestEvenLenPalindrome(int pivot) const {
+        int idx = pivot * 2 ;
+        int left = idx / 2 - d[idx] / 2;
+        int right = left + d[idx] - 1;
+        return right - left + 1;
+    }
+
+    int LongestPalindrome(int pivot) const {
+        return max(LongestOddLenPalindrome(pivot),
+                   LongestEvenLenPalindrome(pivot));
+    }
+
+private:
+    string origin;
+    string s;
+    vector<int> d;
+};
+
+string solve(const string& str) {
+    Manacher m(str);
+    int n = str.size();
 
     set<int> st_l, st_r;
-    for (i=0; i<n; i++) {
-        if (!dp[i]) {
-            continue;
-        }
-        int left = i / 2 - dp[i] / 2;
-        int right = left + dp[i] - 1;
-        // print(left, right);
+    for (int i = 0; i < n; i++) {
+        // odd
+        {
+            int len = m.LongestOddLenPalindrome(i);
+            int left = i - len / 2;
+            int right = left + len - 1;
 
-        if (left == 0 && right != (int)str.size() - 1) {
-            // print("L", right);
-            st_l.insert(right);
+            if (left == 0 && right != (int)str.size() - 1) {
+                // print("L", right);
+                st_l.insert(right);
+            }
+            if (right == (int)str.size() - 1 && left != 0) {
+                // print("R", left);
+                st_r.insert(left);
+            }
         }
-        if (right == (int)str.size() - 1 && left != 0) {
-            // print("R", left);
-            st_r.insert(left);
+
+        // even
+        {
+            int len = m.LongestEvenLenPalindrome(i);
+            int left = i - len / 2;
+            int right = left + len - 1;
+
+            if (left == 0 && right != (int)str.size() - 1) {
+                // print("L", right);
+                st_l.insert(right);
+            }
+            if (right == (int)str.size() - 1 && left != 0) {
+                // print("R", left);
+                st_r.insert(left);
+            }
         }
     }
 
@@ -92,7 +143,7 @@ void manacher(const string& str) {
             ans = min(ans, num);
         }
     }
-    print(str.substr(0, ans + 1));
+    return str.substr(0, ans + 1);
 }
 
 int main() {
@@ -105,8 +156,7 @@ int main() {
         input(n);
         input(s);
 
-        printf("Case #%d: ", case_ + 1);
-        manacher(s);
+        printf("Case #%d: %s\n", case_ + 1, solve(s).c_str());
     }
 
     return 0;
