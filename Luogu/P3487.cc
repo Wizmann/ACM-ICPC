6a329393f6175d96d2dd763b6ad0fd53f0000a4b
Wizmann <mail.kuuy@gmail.com>
2024-01-14 15:57:04 +0800

    Create P3487.cc

---
+++ Luogu/P3487.cc
@@ -0,0 +1,274 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <deque>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+typedef long long llint;
+
+const int INF = 0x3f3f3f3f;
+const llint INFLL = 0x3f3f3f3f3f3f3f3fLL;
+
+void print() { cout << "\n"; }
+
+template <typename...T, typename X>
+void print(X&& x, T... args) { cout << x << " "; print(args...); }
+
+int input() { return 0; }
+
+template <typename...T, typename X>
+int input(X& x, T&... args) {
+    if (!(cin >> x)) return 0;
+    return input(args...) + 1;
+}
+
+
+#define MAGIC_BEGIN -435634223
+#define MAGIC_END -324556462
+
+#define MIN_K 1
+#define MAX_K 1000000
+#define MAX_N 15000000
+#define MIN_A 1
+#define MAX_A 1000000000
+#define MIN_TYP 1
+#define MAX_TYP 3
+#define MIN_PAR 0
+#define MAX_PAR 1000000000
+
+#define ERROR 0
+#define CORRECT 1
+
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+static int init = 0; // czy zostala juz wywolana funkcja inicjuj()
+static int lib_n; // ile biblioteka podala juz liczb
+static int con_k; // ile zawodnik podal liczb
+
+static int N, K, A, TYP, PAR; // parametry testu wczytywane z pliku
+static int bre, len_sub, bou, is_end; // zmienne pomocnicze
+
+static int rand2_status = 198402041;
+
+static inline int rand2(int a, int b){
+  rand2_status = rand2_status * 1103515245 + 12345;
+  int x = rand2_status;
+  if (x < 0) x = -x; // -2^31 sie nie zdarza :D
+  x >>= 1;
+  x = a + x % (b - a + 1);
+  return x;
+}
+
+/* test losowy */
+static inline int random_test()
+{
+    return rand2(1, A);
+}
+
+/* test z dlugim podciagiem nierosnacym */
+static inline int decreasing_test()
+{
+    int tmp;
+    if(bre == 0) {
+        bre = rand2(0, (N - lib_n + 1 - len_sub));
+        tmp = A;
+        A -= rand2(0, (A - 1) / len_sub);
+        len_sub--;
+    }
+    else {
+        bre--;
+        tmp = rand2(1, A);
+    }
+    return tmp;
+}
+
+/* test z dlugim podciagiem niemalejacym */
+static inline int increasing_test()
+{
+    return bou - decreasing_test();
+}
+
+static void finish(int res, const char *com)
+{
+    if(res == ERROR)
+        puts(com);
+    exit(0);
+}
+
+/* Inicjuje dane wejsciowe i zwraca liczbe projektow */
+int inicjuj()
+{
+    if(init == 1)
+        finish(ERROR, "Program zawodnika moze wywolac funkcje inicjuj tylko raz!!!");
+    init = 1;
+    scanf("%d", &K);
+    if (K > 0){
+      TYP = 0;
+      N = MAX_N + 2;
+      return K;
+    }
+    int magic_begin, magic_end;
+    scanf("%d%d", &magic_begin, &TYP);
+    if(magic_begin != MAGIC_BEGIN || TYP < MIN_TYP || TYP > MAX_TYP)
+        finish(ERROR, "Program zawodnika nie moze korzystac z stdin!!!");
+    scanf("%d%d%d%d", &N, &K, &A, &PAR);
+    if(N < 1 || N > MAX_N || N < K || K > MAX_K || A < MIN_A || A > MAX_A 
+        || PAR < MIN_PAR || PAR > MAX_PAR)
+        finish(ERROR, "Program zawodnika nie moze korzystac z stdin!!!");
+    scanf("%d", &magic_end);
+    if(magic_end != MAGIC_END)
+        finish(ERROR, "Program zawodnika nie moze korzystac z stdin!!!");
+    con_k = 0;
+    lib_n = 0;
+    is_end = 0;
+    if(TYP == 2 || TYP == 3) {
+        len_sub = PAR;
+        bre = 0;
+    }
+    if(TYP == 2)
+        bou = A--;
+    return K;
+}
+
+/* Sluzy do wczytania ciagu reprezentujacego jakosci projektow */
+int wczytaj()
+{
+    if(unlikely(init == 0))
+        finish(ERROR, "Program zawodnika nie wywolal funkcji inicjuj!!!");
+    if(unlikely(lib_n > N || is_end == 1))
+        finish(ERROR, "Program zawodnika wywolal funkcje wczytaj po otrzymaniu informacji o koncu ciagu!!!");
+    if(unlikely(lib_n == N))
+        return 0;
+    lib_n++;
+    switch (TYP) {
+      case 0:
+        scanf("%d", &A);
+        if(A == 0)
+          is_end = 1;
+        return A;
+        break;
+      case 1: return random_test(); break;
+      case 2: return increasing_test(); break;
+      case 3: return decreasing_test(); break;
+      default:
+              finish(ERROR, "Nieznany typ testu");
+    }
+    return -1;
+}
+
+/* Sluzy do wypisania wyznaczonego podciagu */
+void wypisz(int jakoscProjektu)
+{
+    if(init == 0)
+        finish(ERROR, "Program zawodnika nie wywolal funkcji inicjuj!!!");
+    printf("%d\n", jakoscProjektu);
+    if(++con_k == K)
+        finish(CORRECT, "");
+}
+
+
+int main() {
+    int k = inicjuj();
+
+    vector<int> ns;
+    ns.reserve(16000000);
+
+    int a;
+    while (true) {
+        a = wczytaj();
+        if (!a) {
+            break;
+        }
+        ns.push_back(a);
+    }
+
+    deque<int> dq;
+    vector<int> res;
+
+    int n = ns.size();
+    int cnt = 0;
+    for (int i = 0; i <= n; i++) {
+        int rem = n - i;
+        if (cnt + rem < k) {
+            res.push_back(dq.front());
+            dq.pop_front();
+            cnt++;
+        }
+
+        if (i == n) {
+            break;
+        }
+
+        int cur = ns[i];
+        while (!dq.empty() && cur > dq.back()) {
+            dq.pop_back();
+        }
+        dq.push_back(cur);
+    }
+
+    for (auto num : res) {
+        wypisz(num);
+    }
+
+    return 0;
+}
+
+/*
+
+^^^TEST^^^
+3
+12 5 8 3 15 8 0
+-----
+12
+15
+8
+$$$TEST$$$
+
+^^^TEST^^^
+3
+1 2 3 0
+-----
+1
+2
+3
+$$$TEST$$$
+
+^^^TEST^^^
+3
+1 2 3 4 0
+-----
+2
+3
+4
+$$$TEST$$$
+
+^^^TEST^^^
+3
+99 999 9 9 99 99 0
+-----
+999
+99
+99
+$$$TEST$$$
+
+^^^TEST^^^
+3
+5 4 1 2 3 0
+-----
+5
+4
+3
+$$$TEST$$$
+
+*/
