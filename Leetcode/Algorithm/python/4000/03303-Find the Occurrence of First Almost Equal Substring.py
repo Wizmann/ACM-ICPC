eeb603c4ec89042d91a1c83b5249d7338e4c8a32
Wizmann <mail.kuuy@gmail.com>
2024-10-03 14:44:45 +0800

    Update 03303-Find the Occurrence of First Almost Equal Substring.py

--- Leetcode/Algorithm/python/4000/03303-Find the Occurrence of First Almost Equal Substring.py
+++ Leetcode/Algorithm/python/4000/03303-Find the Occurrence of First Almost Equal Substring.py
@@ -31,3 +31,64 @@ class Solution(object):
             if h in st:
                 return i - m + 1
         return -1
+
+############################
+
+def z_function(s):
+    n = len(s)
+    z = [0] * n
+    l, r = 0, 0
+    for i in xrange(1, n):
+        if i <= r and z[i - l] < r - i + 1:
+            z[i] = z[i - l]
+        else:
+            z[i] = max(0, r - i + 1)
+            while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+        if i + z[i] - 1 > r:
+            l = i
+            r = i + z[i] - 1
+    return z
+
+class Solution(object):
+    def minStartingIndex(self, s, pattern):
+        n = len(pattern)
+        m = len(s)
+        s1 = pattern + '#' + s
+        z1 = z_function(s1)
+        s2 = pattern[::-1] + '#' + s[::-1]
+        z2 = z_function(s2)
+
+        # bcdffg#abcdefg
+        # gffdcb#gfedcba
+
+        print z1, z2
+
+        for i in xrange(n + 1, n + m + 1):
+            a = z1[i]
+            if a == len(pattern):
+                return i - n - 1
+            j = n + 1 + (m + n - (i + n - 1))
+            b = z2[j]
+            if j >= n + 1 and a + b >= n - 1:
+                return i - n - 1
+        return -1
+
+'''
+^^^^^^^^^TEST^^^^^^^^^
+"jkjkkjjkjjkkjk"
+"kkkkk"
+-------------
+-1
+$$$$$$$$TEST$$$$
+
+^^^^^^^^^TEST^^^^^^^^^
+"abcdefg"
+"bcdffg"
+-------------
+1
+$$$$$$$$TEST$$$$
+'''
+
+
+
