94408964b489f84878ce3705b7d70a6d9713021d
Wizmann <mail.kuuy@gmail.com>
2024-10-19 11:31:28 +0800

    Update Z-algorithm.cc

--- Exemplars/字符串/Z-algorithm.cc
+++ Exemplars/字符串/Z-algorithm.cc
@@ -3,17 +3,17 @@
 则 z 被称为 s 的 Z 函数。特别地，z[0] = 0。
 */
 
-vector<int> z_function(string s) {
-  int n = (int)s.length();
-  vector<int> z(n);
-  for (int i = 1, l = 0, r = 0; i < n; ++i) {
-    if (i <= r && z[i - l] < r - i + 1) {
-      z[i] = z[i - l];
-    } else {
-      z[i] = max(0, r - i + 1);
-      while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+vector<int> z_function(const string& s) {
+    int n = (int)s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r && z[i - l] < r - i + 1) {
+            z[i] = z[i - l];
+        } else {
+            z[i] = max(0, r - i + 1);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        }
+        if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
     }
-    if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
-  }
-  return z;
+    return z;
 }
