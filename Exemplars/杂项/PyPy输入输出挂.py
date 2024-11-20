7fd369d48a7d6ade0076b06284798a7f96b3aa4b
Wizmann <mail.kuuy@gmail.com>
2024-11-20 23:38:05 +0800

    Update PyPy输入输出挂.py

--- Exemplars/杂项/PyPy输入输出挂.py
+++ Exemplars/杂项/PyPy输入输出挂.py
@@ -1,89 +1,43 @@
 import sys
-if sys.subversion[0] == "PyPy":
-    import io, atexit
-    sys.stdout = io.BytesIO()
-    atexit.register(lambda: sys.__stdout__.write(sys.stdout.getvalue()))
-    
-    sys.stdin = io.BytesIO(sys.stdin.read())
-    input = lambda: sys.stdin.readline().rstrip()
+import io
 
-st = set()
+# 检查是否为 PyPy
+if sys.implementation.name == "pypy":
+    import atexit
 
-N = 2
+    # 根据 Python 版本区分处理方式
+    if sys.version_info[0] == 2:  # PyPy2
+        original_stdout = sys.stdout
+        original_stdin = sys.stdin
 
-first = []
+        # 替换 stdout
+        sys.stdout = io.BytesIO()
 
-ptr = 1
-for i in xrange(N):
-    while ptr in st:
-        ptr += 1
-    a = ptr
-    ptr += 1
-    qtr = ptr
-    while True:
-        while qtr in st:
-            qtr += 1
-        if (qtr ^ a) not in st:
-            b = qtr
-            break
-        else:
-            qtr += 1
-    c = a ^ b
-    assert c not in st
-    st |= set([a, b, c])
-    first += [a, b, c]
-    '''
-    print "{:10b}".format(a)
-    print "{:10b}".format(b)
-    print "{:10b}".format(c)
-    print '--'
-    '''
+        # 在程序退出时写回原始 stdout
+        def flush_stdout():
+            original_stdout.write(sys.stdout.getvalue())
+            original_stdout.flush()
 
-def solve(n):
-    m = (n + 2) / 3
-    if m == 1:
-        return (1, 2, 3)[(n - 1) % 3]
+        atexit.register(flush_stdout)
 
-    group = m - 1
+        # 替换 stdin
+        sys.stdin = io.BytesIO(sys.stdin.read())
+        input = lambda: sys.stdin.readline().rstrip()  # 定义兼容的 input() 函数
 
-    u = 1
-    while m > u:
-        m -= u
-        u = u * 4
+    elif sys.version_info[0] == 3:  # PyPy3
+        original_stdout = sys.stdout
+        original_stdin = sys.stdin
 
-    # print u, m
+        # 替换 stdout
+        sys.stdout = io.StringIO()
 
-    m -= 1
-    a = u + m
+        # 在程序退出时写回原始 stdout
+        def flush_stdout():
+            original_stdout.write(sys.stdout.getvalue())
+            original_stdout.flush()
 
-    perm = [ ('00', '00', '00'), ('01', '10', '11'), ('10', '11', '01'), ('11', '01', '10')]
+        atexit.register(flush_stdout)
 
-    bits = (len('{:b}'.format(a)) - 1) / 2
-
-    b = ''
-    c = ''
-    for i in xrange(bits):
-        p = perm[m % 4]
-        b = p[1] + b
-        c = p[2] + c
-        m /= 4
-    b = int('10' + b, 2)
-    c = int('11' + c, 2)
-    return (a, b, c)[(n - 1) % 3]
-
-assert solve(8) == 10
-assert solve(7) == 5
-assert solve(4) == 4
-assert solve(3) == 3
-assert solve(1) == 1
-
-for i, u in enumerate(first):
-    v = solve(i + 1)
-    assert u == v
-
-
-if __name__ == '__main__':
-    T = int(raw_input())
-    for case_ in xrange(T):
-        n = int(raw_input())
-        print solve(n)
+        # 替换 stdin
+        sys.stdin = io.StringIO(sys.stdin.read())  # 确保使用文本流
+        input = lambda: sys.stdin.readline().rstrip()  # 定义兼容的 input() 函数
